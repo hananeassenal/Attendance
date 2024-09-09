7c1d5b44054cbf18a65f6d411eb43deb856771d0
hananeassenal <158520265+hananeassenal@users.noreply.github.com>
2024-09-09 13:28:50 +0100

    Update Premon2.py

--- Premon2.py
+++ Premon2.py
@@ -1,244 +1,157 @@
-#######################################################
-import uuid ## random id generator
+import streamlit as st
+import uuid
 from streamlit_option_menu import option_menu
+import cv2
+import numpy as np
+import pandas as pd
+import face_recognition
 from settings import *
-#######################################################
-## Disable Warnings
-#st.set_option('deprecation.showPyplotGlobalUse', False)
-#st.set_option('deprecation.showfileUploaderEncoding', False)
-################################################### Defining Static Data ###############################################
 
+st.set_page_config(page_title="Presence Monitoring Webapp", page_icon="👥", layout="wide")
 
-user_color      = '#bddc6d'
-title_webapp    = "Presence Monitoring Webapp"
+@st.cache_data
+def load_database():
+    return initialize_data()
 
-html_temp = f"""
-            <div style="background-color:{user_color};padding:12px">
-            <h1 style="color:white;text-align:center;">{title_webapp}
-            </h1>
-            </div>
-            """
-st.markdown(html_temp, unsafe_allow_html=True)
+@st.cache_resource
+def process_image(image_array):
+    face_locations = face_recognition.face_locations(image_array)
+    encodesCurFrame = face_recognition.face_encodings(image_array, face_locations)
+    return face_locations, encodesCurFrame
 
-###################### Defining Static Paths ###################4
-if st.sidebar.button('Click to Clear out all the data'):
-    ## Clearing operator Database
-    shutil.rmtree(OPERATORS_DB, ignore_errors=True)
-    os.mkdir(OPERATORS_DB)
-    ## Clearing operator History
-    shutil.rmtree(OPERATORS_HISTORY, ignore_errors=True)
-    os.mkdir(OPERATORS_HISTORY)
-
-if not os.path.exists(OPERATORS_DB):
-    os.mkdir(OPERATORS_DB)
-
-if not os.path.exists(OPERATORS_HISTORY):
-    os.mkdir(OPERATORS_HISTORY)
-# st.write(OPERATORS_HISTORY)
-########################################################################################################################
 def main():
-    ###################################################
+    st.markdown(
+        f"""
+        <div style="background-color:#bddc6d;padding:12px">
+        <h1 style="color:white;text-align:center;">Presence Monitoring Webapp</h1>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
     st.sidebar.header("About")
-    st.sidebar.info("This webapp monitors the presence of operators in a smart factory"
-                    "Webapp using 'Face Recognition' and Streamlit")
-    ###################################################
+    st.sidebar.info("This webapp monitors the presence of operators in a smart factory using 'Face Recognition' and Streamlit")
+
+    if st.sidebar.button('Clear all data'):
+        clear_data()
 
     selected_menu = option_menu(None,
         ['Operator Validation', 'View Operator History', 'Add to Database'],
         icons=['camera', "clock-history", 'person-plus'],
-        ## icons from website: https://icons.getbootstrap.com/
         menu_icon="cast", default_index=0, orientation="horizontal")
 
     if selected_menu == 'Operator Validation':
-        ## Generates a Random ID for image storage
-        operator_id = uuid.uuid1()
-
-        ## Reading Camera Image
-        img_file_buffer = st.camera_input("Take a picture")
-
-        if img_file_buffer is not None:
-            bytes_data = img_file_buffer.getvalue()
-
-            # convert image from opened file to np.array
-            image_array         = cv2.imdecode(np.frombuffer(bytes_data,
-                                                             np.uint8),
-                                               cv2.IMREAD_COLOR)
-            image_array_copy    = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-            # st.image(cv2_img)
-
-            ## Saving operator History
-            with open(os.path.join(OPERATORS_HISTORY,
-                                   f'{operator_id}.jpg'), 'wb') as file:
-                file.write(img_file_buffer.getbuffer())
-                st.success('Image Saved Successfully!')
-
-                ## Validating Image
-                # Detect faces in the loaded image
-                max_faces   = 0
-                rois        = []  # region of interests (arrays of face areas)
-
-                ## To get location of Face from Image
-                face_locations  = face_recognition.face_locations(image_array)
-                ## To encode Image to numeric format
-                encodesCurFrame = face_recognition.face_encodings(image_array,
-                                                                  face_locations)
-
-                ## Generating Rectangle Red box over the Image
-                for idx, (top, right, bottom, left) in enumerate(face_locations):
-                    # Save face's Region of Interest
-                    rois.append(image_array[top:bottom, left:right].copy())
-
-                    # Draw a box around the face and label it
-                    cv2.rectangle(image_array, (left, top), (right, bottom), COLOR_DARK, 2)
-                    cv2.rectangle(image_array, (left, bottom + 35), (right, bottom), COLOR_DARK, cv2.FILLED)
-                    font = cv2.FONT_HERSHEY_DUPLEX
-                    cv2.putText(image_array, f"#{idx}", (left + 5, bottom + 25), font, .55, COLOR_WHITE, 1)
-
-                ## Showing Image
-                st.image(BGR_to_RGB(image_array), width=720)
-
-                ## Number of Faces identified
-                max_faces = len(face_locations)
-
-                if max_faces > 0:
-                    col1, col2 = st.columns(2)
-
-                    # select selected faces in the picture
-                    face_idxs = col1.multiselect("Select face#", range(max_faces),
-                                                 default=range(max_faces))
-
-                    ## Filtering for similarity beyond threshold
-                    similarity_threshold = col2.slider('Select Threshold for Similarity',
-                                                         min_value=0.0, max_value=1.0,
-                                                         value=0.5)
-                                                    ## check for similarity confidence greater than this threshold
-
-                    flag_show = False
-
-                    if ((col1.checkbox('Click to proceed!')) & (len(face_idxs)>0)):
-                        dataframe_new = pd.DataFrame()
-
-                        ## Iterating faces one by one
-                        for face_idx in face_idxs:
-                            ## Getting Region of Interest for that Face
-                            roi = rois[face_idx]
-                            # st.image(BGR_to_RGB(roi), width=min(roi.shape[0], 300))
-
-                            # initial database for known faces
-                            database_data = initialize_data()
-                            # st.write(DB)
-
-                            ## Getting Available information from Database
-                            face_encodings  = database_data[COLS_ENCODE].values
-                            dataframe       = database_data[COLS_INFO]
-
-                            # Comparing ROI to the faces available in database and finding distances and similarities
-                            faces = face_recognition.face_encodings(roi)
-                            # st.write(faces)
-
-                            if len(faces) < 1:
-                                ## Face could not be processed
-                                st.error(f'Please Try Again for face#{face_idx}!')
-                            else:
-                                face_to_compare = faces[0]
-                                ## Comparing Face with available information from database
-                                dataframe['distance'] = face_recognition.face_distance(face_encodings,
-                                                                                       face_to_compare)
-                                dataframe['distance'] = dataframe['distance'].astype(float)
-
-                                dataframe['similarity'] = dataframe.distance.apply(
-                                    lambda distance: f"{face_distance_to_conf(distance):0.2}")
-                                dataframe['similarity'] = dataframe['similarity'].astype(float)
-
-                                dataframe_new = dataframe.drop_duplicates(keep='first')
-                                dataframe_new.reset_index(drop=True, inplace=True)
-                                dataframe_new.sort_values(by="similarity", ascending=True)
-
-                                dataframe_new = dataframe_new[dataframe_new['similarity'] > similarity_threshold].head(1)
-                                dataframe_new.reset_index(drop=True, inplace=True)
-
-                                if dataframe_new.shape[0]>0:
-                                    (top, right, bottom, left) = (face_locations[face_idx])
-
-                                    ## Save Face Region of Interest information to the list
-                                    rois.append(image_array_copy[top:bottom, left:right].copy())
-
-                                    # Draw a Rectangle Red box around the face and label it
-                                    cv2.rectangle(image_array_copy, (left, top), (right, bottom), COLOR_DARK, 2)
-                                    cv2.rectangle(image_array_copy, (left, bottom + 35), (right, bottom), COLOR_DARK, cv2.FILLED)
-                                    font = cv2.FONT_HERSHEY_DUPLEX
-                                    cv2.putText(image_array_copy, f"#{dataframe_new.loc[0, 'Name']}", (left + 5, bottom + 25), font, .55, COLOR_WHITE, 1)
-
-                                    ## Getting Name of operator
-                                    name_operator = dataframe_new.loc[0, 'Name']
-                                    attendance(operator_id, name_operator)
-
-                                    flag_show = True
-
-                                else:
-                                    st.error(f'No Match Found for the given Similarity Threshold! for face#{face_idx}')
-                                    st.info('Please Update the database for a new person or click again!')
-                                    attendance(operator_id, 'Unknown')
-
-                        if flag_show == True:
-                            st.image(BGR_to_RGB(image_array_copy), width=720)
-
-                else:
-                    st.error('No human face detected.')
-
-    if selected_menu == 'View Operator History':
+        operator_validation()
+    elif selected_menu == 'View Operator History':
         view_attendace()
+    elif selected_menu == 'Add to Database':
+        add_to_database()
 
-    if selected_menu == 'Add to Database':
-        col1, col2, col3 = st.columns(3)
+def clear_data():
+    for path in [OPERATORS_DB, OPERATORS_HISTORY]:
+        if os.path.exists(path):
+            shutil.rmtree(path)
+        os.makedirs(path)
+    st.sidebar.success("All data cleared!")
 
-        face_name  = col1.text_input('Name:', '')
-        pic_option = col2.radio('Upload Picture',
-                                options=["Upload a Picture",
-                                         "Click a picture"])
+def operator_validation():
+    operator_id = uuid.uuid1()
+    img_file_buffer = st.camera_input("Take a picture")
 
-        if pic_option == 'Upload a Picture':
-            img_file_buffer = col3.file_uploader('Upload a Picture',
-                                                 type=allowed_image_type)
-            if img_file_buffer is not None:
-                # To read image file buffer with OpenCV:
-                file_bytes = np.asarray(bytearray(img_file_buffer.read()),
-                                        dtype=np.uint8)
+    if img_file_buffer is not None:
+        bytes_data = img_file_buffer.getvalue()
+        image_array = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+        image_array_copy = image_array.copy()
 
-        elif pic_option == 'Click a picture':
-            img_file_buffer = col3.camera_input("Click a picture")
-            if img_file_buffer is not None:
-                # To read image file buffer with OpenCV:
-                file_bytes = np.frombuffer(img_file_buffer.getvalue(),
-                                           np.uint8)
+        save_operator_history(operator_id, img_file_buffer)
 
-        if ((img_file_buffer is not None) & (len(face_name) > 1) &
-                st.button('Click to Save!')):
-            # convert image from opened file to np.array
-            image_array = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-            # st.write(image_array)
-            # st.image(cv2_img)
+        face_locations, encodesCurFrame = process_image(image_array)
 
-            with open(os.path.join(OPERATORS_DB,
-                                   f'{face_name}.jpg'), 'wb') as file:
-                file.write(img_file_buffer.getbuffer())
-                # st.success('Image Saved Successfully!')
+        if face_locations:
+            process_faces(image_array, image_array_copy, face_locations, encodesCurFrame, operator_id)
+        else:
+            st.error('No human face detected.')
 
-            face_locations = face_recognition.face_locations(image_array)
-            encodesCurFrame = face_recognition.face_encodings(image_array,
-                                                              face_locations)
+def save_operator_history(operator_id, img_file_buffer):
+    with open(os.path.join(OPERATORS_HISTORY, f'{operator_id}.jpg'), 'wb') as file:
+        file.write(img_file_buffer.getbuffer())
+    st.success('Image Saved Successfully!')
 
-            df_new = pd.DataFrame(data=encodesCurFrame,
-                                  columns=COLS_ENCODE)
-            df_new[COLS_INFO] = face_name
-            df_new = df_new[COLS_INFO + COLS_ENCODE].copy()
+def process_faces(image_array, image_array_copy, face_locations, encodesCurFrame, operator_id):
+    for idx, (top, right, bottom, left) in enumerate(face_locations):
+        cv2.rectangle(image_array, (left, top), (right, bottom), COLOR_DARK, 2)
+        cv2.rectangle(image_array, (left, bottom + 35), (right, bottom), COLOR_DARK, cv2.FILLED)
+        cv2.putText(image_array, f"#{idx}", (left + 5, bottom + 25), cv2.FONT_HERSHEY_DUPLEX, .55, COLOR_WHITE, 1)
 
-            # st.write(df_new)
-            # initial database for known faces
-            DB = initialize_data()
-            add_data_db(df_new)
+    st.image(BGR_to_RGB(image_array), width=720)
 
-#######################################################
+    col1, col2 = st.columns(2)
+    face_idxs = col1.multiselect("Select face#", range(len(face_locations)), default=range(len(face_locations)))
+    similarity_threshold = col2.slider('Select Threshold for Similarity', min_value=0.0, max_value=1.0, value=0.5)
+
+    if st.button('Process Selected Faces') and face_idxs:
+        process_selected_faces(face_idxs, face_locations, encodesCurFrame, image_array_copy, similarity_threshold, operator_id)
+
+def process_selected_faces(face_idxs, face_locations, encodesCurFrame, image_array_copy, similarity_threshold, operator_id):
+    database_data = load_database()
+    face_encodings = database_data[COLS_ENCODE].values
+    dataframe = database_data[COLS_INFO]
+
+    for face_idx in face_idxs:
+        face_encode = encodesCurFrame[face_idx]
+        dataframe['distance'] = face_recognition.face_distance(face_encodings, face_encode)
+        dataframe['similarity'] = dataframe['distance'].apply(face_distance_to_conf)
+        
+        dataframe_new = dataframe[dataframe['similarity'] > similarity_threshold].sort_values(by="similarity", ascending=False).head(1)
+
+        if not dataframe_new.empty:
+            name_operator = dataframe_new.iloc[0]['Name']
+            attendance(operator_id, name_operator)
+            draw_face_box(image_array_copy, face_locations[face_idx], name_operator)
+        else:
+            st.error(f'No Match Found for the given Similarity Threshold for face#{face_idx}')
+            attendance(operator_id, 'Unknown')
+
+    st.image(BGR_to_RGB(image_array_copy), width=720)
+
+def draw_face_box(image, face_location, name):
+    top, right, bottom, left = face_location
+    cv2.rectangle(image, (left, top), (right, bottom), COLOR_DARK, 2)
+    cv2.rectangle(image, (left, bottom + 35), (right, bottom), COLOR_DARK, cv2.FILLED)
+    cv2.putText(image, f"#{name}", (left + 5, bottom + 25), cv2.FONT_HERSHEY_DUPLEX, .55, COLOR_WHITE, 1)
+
+def add_to_database():
+    col1, col2, col3 = st.columns(3)
+    face_name = col1.text_input('Name:', '')
+    pic_option = col2.radio('Upload Picture', options=["Upload a Picture", "Click a picture"])
+
+    if pic_option == 'Upload a Picture':
+        img_file_buffer = col3.file_uploader('Upload a Picture', type=ALLOWED_IMAGE_TYPE)
+    else:
+        img_file_buffer = col3.camera_input("Click a picture")
+
+    if img_file_buffer and face_name and st.button('Save to Database'):
+        process_new_face(img_file_buffer, face_name)
+
+def process_new_face(img_file_buffer, face_name):
+    file_bytes = np.frombuffer(img_file_buffer.getvalue(), np.uint8)
+    image_array = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+
+    with open(os.path.join(OPERATORS_DB, f'{face_name}.jpg'), 'wb') as file:
+        file.write(img_file_buffer.getbuffer())
+
+    face_locations, encodesCurFrame = process_image(image_array)
+
+    if encodesCurFrame:
+        df_new = pd.DataFrame(data=encodesCurFrame, columns=COLS_ENCODE)
+        df_new[COLS_INFO] = face_name
+        df_new = df_new[COLS_INFO + COLS_ENCODE].copy()
+        if add_data_db(df_new):
+            st.success(f"{face_name} added to the database successfully!")
+        else:
+            st.error("Failed to add to database. Please try again.")
+    else:
+        st.error("No face detected in the image. Please try again with a clear face image.")
+
 if __name__ == "__main__":
     main()
-#######################################################
